a8640afca206bc2f63680711b6830f70dd04b537
Matthew <110512637+Qceww@users.noreply.github.com>
2024-01-22 15:00:18 +0700

    Lanjutin Home Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,38 +35,193 @@ class _HomePageState extends State<HomePage> {
                           "Luxury",
                           style: GoogleFonts.zenAntique(
                             textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w300
-
-                            ),
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300),
                           ),
                         ),
                         Text(
                           "Fashion",
                           style: GoogleFonts.zenAntique(
                             textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w300
-                            ),
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300),
                           ),
                         ),
                         Text(
                           "Series",
                           style: GoogleFonts.zenAntique(
                             textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w300
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black.withOpacity(0.5),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: const BorderSide(),
+                              )),
+                          child: Text(
+                            "EXPLORE COLLECTION",
+                            style: GoogleFonts.tenorSans(
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w300),
                             ),
                           ),
                         ),
-                        ElevatedButton(onPressed: onPressed, child: child)
+                        const Image(
+                            image: AssetImage(
+                                'images/Home_page_1_Indicator_1.png')),
+                        const SizedBox(
+                          height: 10,
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                          ),
+                        )
                       ],
                     ),
                   ),
-                )
+                ),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height - 100,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            'NEW ARRIVAL',
+                            style: GoogleFonts.tenorSans(
+                              textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                          ),
+                          const Image(
+                            image: AssetImage('images/Home_page_garis.png'),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'All',
+                                  style: GoogleFonts.tenorSans(
+                                    textStyle: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Apparel',
+                                  style: GoogleFonts.tenorSans(
+                                    textStyle: const TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Dress',
+                                  style: GoogleFonts.tenorSans(
+                                    textStyle: const TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Tshirt',
+                                  style: GoogleFonts.tenorSans(
+                                    textStyle: const TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Bag',
+                                  style: GoogleFonts.tenorSans(
+                                    textStyle: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                              ],
+                            ),
+                          ),
+                           Column(
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width / 2,
+                                    child: const Center(
+                                      child: Column(
+                                        children: [
+                                          Image(
+                                            image: AssetImage(
+                                                'images/Home_page_new_arrival_1.png'),
+                                          ),
+                                          Text(
+                                              '21WN reversible angora cardigan', textAlign: TextAlign.center,),
+                                          Text('\$120')
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width / 2,
+                                    child: const Center(
+                                      child: Column(
+                                        children: [
+                                          Image(
+                                            image: AssetImage(
+                                                'images/Home_page_new_arrival_1.png'),
+                                          ),
+                                          Text(
+                                              '21WN reversible angora cardigan', textAlign: TextAlign.center,),
+                                          Text('\$120'),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(),
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
               ],
             ),
           ),
